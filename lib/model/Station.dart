6d394bc809c4a125a61b5d9f75e46b269255dfd8
israel <ioluyomi@gmail.com>
2024-01-13 15:27:24 +0000

    added the model station

---
+++ lib/model/Station.dart
@@ -0,0 +1,13 @@
+class Station {
+final String name;
+final String logoUrl;
+final String description;
+final String streamUrl;
+
+Station({
+required this.name,
+required this.logoUrl,
+required this.description,
+required this.streamUrl,
+});
+}
